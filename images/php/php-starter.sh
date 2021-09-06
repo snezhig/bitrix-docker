9dbd573565b952a324cfb82c088f9d66fe942378
snezhig <sneqastk@gmail.com>
2021-09-06 18:43:31 +0300

    Upgrade to 2 version
    - php starts by supervisord
    - removed config for xdebug lower than 3
    - remove some ask for database
    - optimize init.sh

--- images/php/php-starter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$SET_UP_SSH_SERVER" = 'true' ]; then
-  supervisord -n
-else
-  cron && php-fpm
-fi
--- init.sh
+++ init.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" || exit
+function get_dc_env() {
+  sed -n -e "/${1}/ s/.*\= *//p" .env
+}
 
+function fill_placeholder() {
+  sed -ri "s~$1~$2~" .env
+}
+
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" || exit
 ENV_FILE=.env
 #Check if env file exists and offer to recreate it
 if [ -f "$ENV_FILE" ]; then
   echo "The .env file already exists."
-  read -p "Would you like to remove it? (Y|N): " -i 'N' -e REMOVE_ENV_FILE
+  read -r -p "Would you like to remove it? (Y|N): " -i 'N' -e REMOVE_ENV_FILE
   if [ "${REMOVE_ENV_FILE^^}" == 'Y' ]; then
     rm .env
     cp .env.template .env
@@ -19,34 +26,42 @@ fi
 CURRENT_DIR=$PWD
 PARENT_DIR=$(dirname "$PWD")
 
-read -p "Type the project path: " -i "$PARENT_DIR" -e PROJECT_PATH
-sed -ri "s~@PATH_TO_PROJECT@~$PROJECT_PATH~" .env
+if grep -q @PATH_TO_PROJECT@ ".env"; then
+  read -r -p "Type the project path: " -i "$PARENT_DIR" -e PROJECT_PATH
+  fill_placeholder "@PATH_TO_PROJECT@" "$PROJECT_PATH"
+else
+  PROJECT_PATH=$(get_dc_env PROJECT_PATH)
+fi
 
-PROJECT_NAME=${PROJECT_PATH%*/}
-read -p "Type the project name: " -i "$(sed s~-~_~g <<<${PROJECT_NAME##*/})" -e PROJECT_NAME
-sed -ri "s~@YOUR_PROJECT_NAME@~$PROJECT_NAME~" .env
+COMPOSE_PROJECT_NAME=${PROJECT_PATH%*/}
 
-read -p "Type the domain for project: " -i "${PROJECT_NAME}.loc" -e PROJECT_DOMAIN
-sed -ri "s~@YOUR_PROJECT_DOMAIN@~$PROJECT_DOMAIN~" .env
+if grep -q @YOUR_PROJECT_NAME@ ".env"; then
+  read -r -p "Type the project name: " -i "$(sed s~-~_~g <<<${COMPOSE_PROJECT_NAME##*/})" -e COMPOSE_PROJECT_NAME
+  fill_placeholder "@YOUR_PROJECT_NAME@" "$COMPOSE_PROJECT_NAME"
+else
+  COMPOSE_PROJECT_NAME=$(get_dc_env COMPOSE_PROJECT_NAME)
+fi
 
-read -p "Type the path to docker data: " -i "$CURRENT_DIR/data" -e PATH_TO_DOCKER_DATA
-sed -ri "s~@PATH_TO_DOCKER_DATA@~$PATH_TO_DOCKER_DATA~" .env
+if grep -q @YOUR_PROJECT_DOMAIN@ ".env"; then
+  read -r -p "Type the domain for project: " -i "${COMPOSE_PROJECT_NAME}.loc" -e PROJECT_DOMAIN
+  fill_placeholder "@YOUR_PROJECT_DOMAIN@" "$PROJECT_DOMAIN"
+else
+  PROJECT_DOMAIN=$(get_dc_env DOMAIN)
+fi
 
-read -p "Type the log path: " -i "$CURRENT_DIR/logs" -e LOGS_PATH
-sed -ri "s~@PATH_TO_LOGS@~$LOGS_PATH~" .env
+if grep -q @PATH_TO_DOCKER_DATA@ ".env"; then
+  read -r -p "Type the path to docker data: " -i "$CURRENT_DIR/data" -e DATA_PATH
+  fill_placeholder "@PATH_TO_DOCKER_DATA@" "$DATA_PATH"
+else
+  DATA_PATH=$(get_dc_env DATA_PATH)
+fi
 
-read -p "Type database name: " -i "bitrix" -e DB_NAME
-sed -ri "s~@DB_NAME@~$DB_NAME~" .env
-
-read -p "Type database user: " -i "bitrix" -e DB_USER
-sed -ri "s~@DB_USER@~$DB_USER~" .env
-
-read -p "Type database user password: " -i "yourStrongPassword" -e DB_PASSWORD
-sed -ri "s~@DB_PASSWORD@~$DB_PASSWORD~" .env
-
-read -p "Type database root password: " -i "yourStrongPassword" -e DB_ROOT_PASSWORD
-sed -ri "s~@DB_ROOT_PASSWORD@~$DB_ROOT_PASSWORD~" .env
-
+if grep -q @PATH_TO_LOGS@ ".env"; then
+  read -r -p "Type the log path: " -i "$CURRENT_DIR/logs" -e LOGS_PATH
+  fill_placeholder "@PATH_TO_LOGS@" "$LOGS_PATH"
+else
+  LOGS_PATH=$(get_dc_env LOGS_PATH)
+fi
 
 for TMP in "tmp" "tmp/php_sessions"; do
   mkdir -p "./$TMP"
@@ -58,8 +73,15 @@ done
 
 #Create public directory
 if [ ! -d "${PROJECT_PATH}/public" ]; then
-  read -p "Create public folder with index.php? " -i 'Y' -e CREATE_PUBLIC
+  read -r -p "Create public folder with index.php? " -i 'Y' -e CREATE_PUBLIC
   if [ "${CREATE_PUBLIC^^}" == 'Y' ]; then
-    mkdir "${PROJECT_PATH}/public" && echo '<?php phpinfo();' > "${PROJECT_PATH}/public/index.php"
+    mkdir "${PROJECT_PATH}/public" && echo '<?php phpinfo();' >"${PROJECT_PATH}/public/index.php"
   fi
+fi
+
+read -p -r 'Download bitrix_setup.php? ' -i 'Y' -e DBS
+
+if [ "${DBS}" == 'Y' ]; then
+  wget "https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php" -O "${PROJECT_PATH}/public/bitrixsetup.php"
+  echo "File is downloaded."
 fi
